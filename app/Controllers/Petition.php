d7b7d89f7f549bd3c222bbd1eb08a0674a548ce9
Mike <gg.188.gagarin@gmail.com>
2022-09-26 16:37:31 +0300

    refactor cron search

--- app/Controllers/Petition.php
+++ app/Controllers/Petition.php
@@ -16,20 +16,55 @@ class Petition extends BaseController
     {
         $this->petition = new PetitionModel();
         $this->user = new UserModel();
+        $this->userPetition = new UserPetitionModel();
     }
 
-    public function index()
+    public function index($status = false)
     {
-        $petitions = $this->petition->paginate(5);
+        if ($q = $this->request->getVar('q')) {
+            $this->petition->like('name', $q, 'both');
+        }
+        if ($status) {
+            $this->petition->where('status', $status);
+        }
+        if ($mult = $this->request->getVar('mult')) {
+            $statuses = explode(',', $mult);
+            $this->petition
+                ->whereIn('status', $statuses);
+        }
+        $petitions = $this->petition
+            ->select('petition.*, user.firstname, user.lastname')
+            ->join('user', 'petition.user_id = user.id')
+            ->paginate(PetitionModel::PER_PAGE);
         $pager = $this->petition->pager;
-        return view('index', ['petitions' => $petitions, 'pager' => $pager]);
+        if ($this->request->isAJAX()) {
+            return view('petition/indexContent',  compact('petitions', 'pager', 'status'));
+        } else {
+            return view('petition/index', compact('petitions', 'pager', 'status'));
+        }
     }
 
-    public function petitionsByUser()
+    public function all($status = false)
     {
-        $petitions = $this->petition->where('user_id', session()->get('user')['id'] )->paginate(5);
-        $pager = $this->petition->pager;
-        return view('dashboard/user_petitions', ['userPetitions' => $petitions, 'pager' => $pager]);
+        $this->petition->where('status !=', PetitionModel::STATUS_DRAFT);
+        if (!UserModel::isAdmin()) {
+            $this->petition->where('status !=', PetitionModel::STATUS_PREMODERATING);
+        };
+        return $this->index($status);
+    }
+
+    public function my($status = false)
+    {
+        $this->petition->where('user_id', session()->get('user')['id']);
+        return $this->index($status);
+    }
+
+    public function mySubs($status = false)
+    {
+        $this->petition
+            ->join('user_petition', 'user_petition.petition_id = petition.id')
+            ->where('user_petition.user_id', session()->get('user')['id']);
+        return $this->index($status);
     }
 
     public function create()
